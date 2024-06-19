0b1d2ee35eb8690a43dad7c3efa6fc138713cec3
kannan-7 <kannan_7@hotmail.com>
2024-06-20 01:08:34 +0800

    commit first

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const aboutHtml = fs.readFileSync("./about.html", "utf-8");
 const experienceHtml = fs.readFileSync("./experience.html", "utf-8");
 const educationHtml = fs.readFileSync("./education.html", "utf-8");
 const contactHtml = fs.readFileSync("./contact.html", "utf-8");
+const notFoundHtml = fs.readFileSync("./404.html", "utf-8");
 
 const server = http.createServer(function (req, res) {
   const url = req.url;
@@ -13,25 +14,25 @@ const server = http.createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(indexHtml);
     res.end();
-  }
-  if (url === "/about") {
+  } else if (url === "/about") {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(aboutHtml);
     res.end();
-  }
-  if (url === "/experience") {
+  } else if (url === "/experience") {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(experienceHtml);
     res.end();
-  }
-  if (url === "/education") {
+  } else if (url === "/education") {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(educationHtml);
     res.end();
-  }
-  if (url === "/contact") {
+  } else if (url === "/contact") {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(contactHtml);
+    res.end();
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write(notFoundHtml);
     res.end();
   }
 });
